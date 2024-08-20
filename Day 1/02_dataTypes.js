df96f0016f9069755c31b7bcf0cffd162d375e96
Jaivin Domadiya <Jaivindomadiya@gmail.com>
2024-08-20 17:39:26 +0530

    added datatype file

--- Day 1/02_dataTypes.js
+++ Day 1/02_dataTypes.js
@@ -4,3 +4,30 @@
 
 console.log(3 + 3);
 console.log("Jaivin domadiya");
+
+let name = "jaivin"
+let age = 21
+let isTrue = true
+
+//number => 2 to power 53
+//bigint
+//string '' or "" both use 
+// boolean => true or false 
+//null=>standalone value  // empty value 
+//undefined => 
+//symbol => uniqueness 
+
+
+//object 
+
+console.log(typeof(age));
+console.log(typeof(name));
+console.log(typeof(null)); // null is object (interview questions)
+
+console.log(typeof(undefined))
+
+
+
+
+
+
