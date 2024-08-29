95a17eb0341a0b6f2149b1311889cc2abb604ac5
Jaivin Domadiya <Jaivindomadiya@gmail.com>
2024-08-29 12:42:01 +0530

    Make one folder

---
+++ Learning/03_datatypes_Conversion.js
@@ -0,0 +1,56 @@
+// let score = 11
+
+// console.log(typeof(score)) 
+
+// let score1 = "33abc"
+// console.log(typeof(score1)) 
+// let ValueInNumber = Number(score1)
+// console.log(typeof ValueInNumber)
+// console.log(ValueInNumber);
+
+
+// let score2 = true
+// console.log(typeof(score2)) 
+// let ValueInNumber1 = Number(score2)
+//console.log(typeof ValueInNumber)
+
+
+// //33 => 3
+// //33abc => NaN (not an number)
+// //true => 1; false =>0
+
+
+// let isLogIn = ""
+// let booleanIsLogIn = Boolean(isLogIn)
+// console.log(booleanIsLogIn)
+
+
+// // 1 => true
+// // 0 => flase 
+
+// let someNumber = 32
+// let stringNumber = String(somenumber)
+// console.log(stringNumber);
+
+//********************* Operations ***********************
+// let value = 3
+// console.log(value)
+// let negvalue = -value
+// console.log(negvalue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**3)
+
+let str1 = "hello"
+let str2 = " Jaivin"
+
+str3 = str1 + str2
+console.log(str3)
+
+console.log(1 + "2");  //12
+console.log("1" + "2"); //12
+console.log("1"+ 2 + 2); //122
+console.log(1 + 2 + "2") //32  ------------
+
---
+++ Learning/04_datatypecomparision.js
@@ -0,0 +1,14 @@
+//console.log("2"> 1);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined == null); //true
+console.log(undefined >= null);
+
+
